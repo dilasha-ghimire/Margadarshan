b3abaf880b8cf189bf315d825589d26b963927cb
Dia505 <dia48261@gmail.com>
2023-12-17 18:04:28 +0545

    login client-side auth

--- js/login.js
+++ js/login.js
@@ -6,19 +6,43 @@ function validateLogin() {
     var password = document.getElementById('password').value;
     var passwordError = document.getElementById("passwordError");
 
-    if (email !== 'user@example.com' || password !== 'password123') {
-      document.getElementById('emailError').textContent = '* Invalid email address';
-      document.getElementById('email').classList.add('error');
-      emailError.className = "error";
-
-      document.getElementById('passwordError').textContent = '* Incorrect password';
-      document.getElementById('password').classList.add('error');
-      passwordError.className = "error";
-    } 
-    else {
-      alert('Login successful!');
-      window.location.href = 'homepage1.html';
+    var loginData = {
+      email: email,
+      password: password
     }
+
+    fetch("http://localhost:8080/api/students", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(loginData)
+    })
+    .then(response => {
+      if(!response.ok) {
+        throw new Error("Network response failure");
+      }
+      return response.json();
+    })
+    .then(result => {
+      if(result.success) {
+        alert('Login successful!');
+        window.location.href = 'homepage1.html';
+      }
+      else {
+        document.getElementById('emailError').textContent = '* Invalid email address';
+        document.getElementById('email').classList.add('error');
+        emailError.className = "error";
+
+        document.getElementById('passwordError').textContent = '* Incorrect password';
+        document.getElementById('password').classList.add('error');
+        passwordError.className = "error";
+      }
+    })
+
+    .catch(error => {
+      console.error("Fetch operation problem: " + error);
+    })
 }
 
 function clearErrors() {
