60c2f334999c61a90e990e3e7454c543cfcedb1f
Anita-aaa <at9941787@gmail.com>
2023-12-27 20:57:17 +0545

    working on exam

--- Margadarshan_frontend/src/App.tsx
+++ Margadarshan_frontend/src/App.tsx
@@ -3,8 +3,11 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import University from "./tsx-files/University.tsx";
 import Scholarship from './tsx-files/Scholarship.tsx';
 import Registration from './tsx-files/Registration.tsx';
-import Login from './tsx-files/Login.tsx'
+import SOP from './tsx-files/SOP.tsx';
+import SOPDialogBox from './tsx-files/SOPDialogBox.tsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import University1 from './tsx-files/University1.tsx';
+
 
 const router = createBrowserRouter(
   [
@@ -21,8 +24,16 @@ const router = createBrowserRouter(
             element: <Registration/>
       },
       {
-            path: "/login",
-            element: <Login/>
+        path:"/sop",
+        element:<SOP/>
+      },
+      {
+        path:"/dialogbox",
+        element:<SOPDialogBox/>
+      },
+      {
+            path: "exam",
+            element: <University1/>
       }
   ]
 )
@@ -32,6 +43,7 @@ const queryClient = new QueryClient();
 function App() {
 
   return (
+
 
       <>   
             <QueryClientProvider client={queryClient}>
