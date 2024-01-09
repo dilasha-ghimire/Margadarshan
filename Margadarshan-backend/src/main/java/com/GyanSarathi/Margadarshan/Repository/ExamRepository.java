3d32be210f4cc9693b2954ff966a7d3a526d7f62
Anukul07 <108445077+Anukul07@users.noreply.github.com>
2024-01-09 17:39:15 +0545

    Exams backend (#71)

--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/Repository/ExamRepository.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/Repository/ExamRepository.java
@@ -1,9 +1,16 @@
 package com.GyanSarathi.Margadarshan.Repository;
 
 import com.GyanSarathi.Margadarshan.entity.Exam;
+import com.GyanSarathi.Margadarshan.entity.ExamDeadlines;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExamRepository extends JpaRepository<Exam,Integer> {
+
+    @Query(value = "SELECT * FROM exams", nativeQuery = true)
+    List<Exam> findAll();
 }
