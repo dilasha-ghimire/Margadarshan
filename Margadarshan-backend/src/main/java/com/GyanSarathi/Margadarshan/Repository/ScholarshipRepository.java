39acaa02d409c5462caad36a0b165326a8cf847a
Anukul07 <anukulnepal2014@gmail.com>
2024-01-14 10:42:45 +0545

    exam-scholarships filtration

--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/Repository/ScholarshipRepository.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/Repository/ScholarshipRepository.java
@@ -13,13 +13,15 @@ import java.util.List;
 @Repository
 public interface ScholarshipRepository extends JpaRepository<Scholarship,Integer> {
 
-
-    @Query("SELECT s FROM Scholarship s WHERE s.grant = :scholarship_grant OR s.scholarshipType = :scholarship_type OR s.scholarshipGpa= :scholarship_gpa")
-    List<Scholarship> findByGrantOrTypeOrGpa(@Param("scholarship_grant") String scholarshipGrant,
-                                              @Param("scholarship_type") String scholarshipType,
-                                              @Param("scholarship_gpa") String scholarshipGpa);
-
     //filter by name
     @Query(value = "SELECT * from Scholarship where scholarship_name = :scholarshipName",nativeQuery = true )
     List<Scholarship> findByName(@Param("scholarshipName") String scholarshipName);
+
+    @Query(value ="SELECT * FROM Scholarship WHERE ((CAST(scholarship_grant AS SIGNED) <= ?1 AND CAST(scholarship_grant AS SIGNED) >= ?2))OR scholarship_type = ?3 OR scholarship_gpa = ?4", nativeQuery = true)
+    List<Scholarship> findByGrantOrTypeOrGpa(long grantUpperBound, long grantLowerBound, String scholarshipType, String scholarshipGpa);
+
+    @Query(value = "Select * from Scholarship WHERE scholarship_grant=?1 or scholarship_type=?2 or scholarship_gpa=?3",nativeQuery = true)
+    List<Scholarship> findByGrantOrTypeOrGpaTwo(String scholarshipGrant, String scholarshipType, String scholarshipGpa);
+
 }
+
