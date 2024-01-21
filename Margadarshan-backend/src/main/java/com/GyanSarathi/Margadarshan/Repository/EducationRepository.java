c87f10df8a7d19321ae46f350f45d37b252fed8a
Anukul07 <anukulnepal2014@gmail.com>
2024-01-21 08:53:56 +0545

    Education save functionality

--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/Repository/EducationRepository.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/Repository/EducationRepository.java
@@ -13,7 +13,10 @@ public interface EducationRepository extends JpaRepository<Education,Integer> {
     @Query(value = "SELECT Education.education_id, Education.education_institute, Education.education_qualification, Students.student_id, Students.full_name FROM Education LEFT JOIN Students ON Education.student_id = Students.student_id WHERE Students.student_id = ?1", nativeQuery = true)
     List<?> listOfEducation(int studentId);
 
-    @Query(value ="INSERT INTO Education VALUES (?1,?2,?3)" ,nativeQuery = true)
-    void educationAddOn(String educationInstitute, String educationQualification, int studentId);
+//    @Query(value ="INSERT INTO Education VALUES (?1,?2,?3)" ,nativeQuery = true)
+//    void educationAddOn(String educationInstitute, String educationQualification, int studentId);
+//
+//    @Query(value = "SELECT Education.education_id, Education.education_institute, Education.education_qualification, Students.student_id, Students.full_name FROM Education LEFT JOIN Students ON Education.student_id = Students.student_id WHERE Students.student_id = ?1", nativeQuery = true)
+//    List<Education> listOfEducationTwo(int studentId);
 
 }
--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/EducationService.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/EducationService.java
@@ -1,6 +1,9 @@
 package com.GyanSarathi.Margadarshan.service;
 
 import com.GyanSarathi.Margadarshan.dto.EducationDto;
+import com.GyanSarathi.Margadarshan.dto.ExamDto;
+import com.GyanSarathi.Margadarshan.dto.RoadmapDto;
+import com.GyanSarathi.Margadarshan.entity.Education;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,5 +12,11 @@ import java.util.List;
 public interface EducationService {
     List<?> listOfEducation(EducationDto educationDto);
 
-    void addOnEducation(EducationDto educationDto);
+//    void addOnEducation(EducationDto educationDto);
+//
+//    List<Education> listOfEducationTwo(EducationDto educationDto);
+
+    public String save(EducationDto educationDto);
+
+
 }
