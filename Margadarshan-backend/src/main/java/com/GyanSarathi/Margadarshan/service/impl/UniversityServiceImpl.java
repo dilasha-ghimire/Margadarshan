2ede7719d1732048a347d73bd333832b1ce71765
Anukul07 <anukulnepal2014@gmail.com>
2024-01-25 12:28:46 +0545

    roadmap

--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/impl/UniversityServiceImpl.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/impl/UniversityServiceImpl.java
@@ -148,11 +148,7 @@ public class UniversityServiceImpl implements UniversityService{
 
     @Override
     public University filterForRoadmap(RoadmapDto roadmapDto) {
-<<<<<<< HEAD
-=======
-
->>>>>>> 8da0d4946d6116d95f14a8b90e23f9b629e4a45b
-        if(Objects.equals(roadmapDto.getDegreeSelection(), "masters") && Objects.equals(roadmapDto.getLanguageTestSelection(),"TOEFL")){
+        /*if(Objects.equals(roadmapDto.getDegreeSelection(), "masters") && Objects.equals(roadmapDto.getLanguageTestSelection(),"TOEFL")){
             University MastersUniversityWithToefl =
                     universityRepository.filterForRoadmapTwo
                             (roadmapDto.getUniversityName(),Double.parseDouble(roadmapDto.getAverageMastersGpa()),0,Integer.parseInt(roadmapDto.getToeflScore()),
@@ -179,9 +175,8 @@ public class UniversityServiceImpl implements UniversityService{
 
                                     0, Integer.parseInt(roadmapDto.getSatScore()));
             return BachelorsUniversityWithToefl;
-        }
-
-        return null;
+        }*/
+        return universityRepository.filterForRoadmap(roadmapDto.getUniversityName());
     }
 
     @Override
