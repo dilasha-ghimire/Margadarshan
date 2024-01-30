7767b03f94c48eda6285434ce6019e377eac72b9
Anukul07 <108445077+Anukul07@users.noreply.github.com>
2024-01-30 16:26:45 +0545

    Anukul/valid (#141)
    * scholarship bugs
    
    * exam row count
    
    * Education row count
    
    * sop

--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/dto/SopDto.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/dto/SopDto.java
@@ -1,29 +1,23 @@
 package com.GyanSarathi.Margadarshan.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class SopDto {
 
 
-    @NotNull
     private int sopId;
 
-    @NotNull
     private String sopName;
 
-    @NotNull
     private MultipartFile sopPdf;
 
-    @NotNull
     private int studentId;
 
 
--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/impl/SopServiceImpl.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/impl/SopServiceImpl.java
@@ -3,17 +3,28 @@ package com.GyanSarathi.Margadarshan.service.impl;
 import com.GyanSarathi.Margadarshan.Repository.SopRepository;
 import com.GyanSarathi.Margadarshan.dto.SopDto;
 import com.GyanSarathi.Margadarshan.entity.Sop;
+import com.GyanSarathi.Margadarshan.entity.Student;
 import com.GyanSarathi.Margadarshan.service.SopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class SopServiceImpl implements SopService {
 
     private final SopRepository sopRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Autowired
     public SopServiceImpl(SopRepository sopRepository) {
@@ -23,14 +34,24 @@ public class SopServiceImpl implements SopService {
     @Override
     public Sop saveSOP(SopDto sopDto) throws IOException {
         Sop sop = new Sop();
+        Student student = new Student();
         sop.setSopName(sopDto.getSopName());
-        sop.setSopPdf(sopDto.getSopPdf().getBytes());
-        sop.setStudentId(sopDto.getStudentId());
+        student.setId(sopDto.getStudentId());
+
+        String fileName = UUID.randomUUID().toString()+"_"+ sopDto.getSopPdf().getOriginalFilename();
+        Path filePath = Paths.get(uploadPath,fileName);
+        try {
+            Files.copy(sopDto.getSopPdf().getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        sop.setSopPdf(fileName);
+        sop.setStudent(student);
         return sopRepository.save(sop) ;
     }
 
     @Override
     public List<Sop> findSopById(SopDto sopDto) {
-        return sopRepository.findAllByStudentId(sopDto.getStudentId());
+        return sopRepository.retrieveByStudentId(sopDto.getStudentId());
     }
 }
