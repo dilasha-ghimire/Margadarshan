3c04d6f84d3a050d71448fe0ba166c01f62da266
Anukul07 <108445077+Anukul07@users.noreply.github.com>
2024-01-31 20:52:20 +0545

    Anukul/profilecomp (#151)
    * profile complete
    
    * scholarship fixed
    
    * document complete
    
    * profile complete
    
    * document and sop  complete

--- Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/impl/DocumentServiceImpl.java
+++ Margadarshan-backend/src/main/java/com/GyanSarathi/Margadarshan/service/impl/DocumentServiceImpl.java
@@ -100,4 +100,24 @@ public class DocumentServiceImpl implements DocumentService {
         documentRepository.save(existingDocument);
         return "updated";
     }
+
+    public List<DocumentDto> getAllDocuments() {
+        List<Document> documents = documentRepository.findAll();
+        return mapDocumentsToDtosTwo(documents);
+    }
+
+    public List<DocumentDto> mapDocumentsToDtosTwo(List<Document> documents) {
+        List<DocumentDto> documentDtos = new ArrayList<>();
+
+        for (Document document : documents) {
+            DocumentDto documentDto = DocumentDto.builder()
+                    .documentId(document.getDocumentId())
+                    .documentName(document.getDocumentName())
+                    .studentId(document.getDocumentStudent().getId())
+                    .documentImageString(document.getDocumentImage())
+                    .build();
+            documentDtos.add(documentDto);
+        }
+        return documentDtos;
+    }
 }
